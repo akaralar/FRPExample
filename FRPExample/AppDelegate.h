d16a9422abd2ce7417090116442a244e88ca2ee7
Ahmet Karalar <akaralar@gmail.com>
2014-12-29 02:17:04 +0200

    Initial Commit

---
+++ FRPExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FRPExample
+//
+//  Created by Ahmet Karalar on 29/12/14.
+//  Copyright (c) 2014 Ahmet Karalar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FRPExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FRPExample
+//
+//  Created by Ahmet Karalar on 29/12/14.
+//  Copyright (c) 2014 Ahmet Karalar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
